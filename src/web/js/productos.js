b4170f20d304a1436abb7a3e2eb579c898b46fcb
Edgar <edgarlopezcalomarde@gmail.com>
2022-12-15 20:58:01 +0100

    json productos fase2

--- src/web/js/productos.js
+++ src/web/js/productos.js
@@ -13,7 +13,7 @@ const productos = [
     {
         id: "2jpn8l5kptpga4qge24ipch",
         category: "skincare",
-        name: "SUPER GREEN CONCENTRATE",
+        name: "Super green concentrated",
         sort_description: "Sérum super nutritivo que ayuda a equilibrar pieles con rojeces o acné.",
         big_description: `
             Suero anhidro que contiene Hechy Complex combinado con aceite de semilla de cáñamo combinado con aceite de chía, considerado un “superalimento” antioxidante rico en omega-3 y omega-6, y escualano, un emoliente protector obtenido a través del aceite de oliva.
@@ -31,7 +31,7 @@ const productos = [
     {
         id: "35820h75phr68dn0d8m33mg",
         category: "skincare",
-        name: "RESCUE FACE MIST",
+        name: "Rescue face mist",
         sort_description: "Bruma facial hidratante, antioxidante  y nutritiva que refresca la piel y aporta sensación de confort a cualquier hora del día.",
         big_description: `
         Delicada bruma facial de agua de Rosa Búlgara y una poderosa combinación a base de kale, limón y zanahoria que le aporta propiedades antioxidantes y nutritivas a la piel. Además, contiene extracto de Aloe vera y Niacinamida que refuerzan su efecto hidratante. Refresca la piel y le devuelve su sensación de confort a cualquier hora del día. 
@@ -48,7 +48,7 @@ const productos = [
     {
         id: "q1hc977cmf92gidqq1l8tfo",
         category: "skincare",
-        name: "URBAN FACE MIST",
+        name: "Urban face mist",
         sort_description: "Bruma facial que aporta sensación de hidratación, frescura y protección a la piel a cualquier hora del día.",
         big_description: `
         Bruma facial que aporta sensación de hidratación, frescura y protección a la piel a cualquier hora del día. Suave bruma facial a base de agua de Cannabis Sativa combinada con un polisacárido de  alto peso molecular, que ejerce de escudo protector, invisible y transpirable frente a la polución ambiental debida a la contaminación atmosférica. Además, contiene Niacinamida, que refuerza el efecto protector de la piel al ayudar a reducir los efectos producidos por la luz azul.
@@ -64,18 +64,48 @@ const productos = [
     {
         id: "1ssaee7q71m84d79jppaka1",
         category: "skincare",
-        name: "GLOWY CLEANSING GEL",
+        name: "Glowy cleansing gel",
         sort_description: "Limpiador facial hidratante e iluminador.",
         big_description: `
         Mantén tu piel limpia, hidratada y con un efecto glowy gracias a nuestro Glowy Cleansing Gel. Elimina los restos de maquillaje e impurezas de forma delicada, aportando un extra de hidratación a tu piel para mantenerla sana e iluminada. Formulado con agua termal, extracto de cúrcuma y betaína hidratante extraída de la raíz de la remolacha, este limpiador es el aliado perfecto para tu limpieza facial. 
         95% ingredientes de origen natural /Apto para todo tipo de pieles / Testado dermatológicamente 
         `,
         efficacy: `
-    
+        El 100% de los consumidores confirma que el producto limpia y elimina de manera eficiente la impurezas de la piel y el exceso de sebo dejándola suave y de aspecto saludable.
+        *Eficacia subjetiva en 21 consumidores, 2 veces al día durante 4 semanas de uso.
         `,
         ingredients: "VEGAN, PEG FREE, PARABEN FREE, SILICONE FREE, ALCOHOL FREE, SULFATE FREE",
         price: 28
+    },
+    {
+        id: "29m4trfo2q97maa0e3sip2",
+        category: "bodycare",
+        name: "Hyaluronic Energy Body Serum",
+        sort_description: "Sérum corporal piernas cansadas",
+        big_description: `
+        Sérum en formato de gel. Su fórmula aporta una sensación de alivio y confort inmediatos gracias a 8 activos vegetales: goldenrod, el alga roja, ácido hialurónico vegano, potentes activos naturales marinos.
+        Contribuye al biensestar de la piel, con efecto calmante inmediato, acción antiséptica y antiinflamatoria y además activa la microcirculación y el flujo sanguíneo.
+        Aporta máxima suavidad, firmeza y elasticidad en la piel, sin dejarla pegajosa gracias a su rápida absorción.      
+        `,
+        efficacy: `
+            96% ingredientes de origen natural / Apto para pieles sensibles y atópicas / Apto para embarazadas 
+        `,
+        ingredients: "VEGAN, PARABEN FREE, NON TOXIC, PEG FREE, PEG FREE, MEA FREE, TEA FREE,  SILICONE FREE, ALCOHOL FREE, MINERAL OILS FREE",
+        price: 26
+    },
+    {
+        id: "30l5sm5ffiqmar9jofriai7",
+        category: "bodycare",
+        name: "Hyaluronic Energy Body Serum",
+        sort_description: "Emulsión corporal de rápida extensibilidad y absorción a base de aceite de semilla de cáñamo combinado con agua floral de Cannabis Sativa.",
+        big_description: `
+        Contribuye al bienestar de la piel dejándola suave e hidratada, sin sensación de pegajosidad, una vez absorbida. Actúa como agente calmante de acción rápida. Ayuda a la piel a reducir la pérdida de agua epidérmica. Es altamente nutritiva.
+        `,
+        efficacy: `96% ingredientes de origen natural / Apto para todo tipo de pieles: grasas, sensibles o mixtas`,
+        ingredients: "VEGAN, PARABEN FREE, NON TOXIC, PEG FREE",
+        price: 16
     }
+        
 ]
 
 
@@ -96,54 +126,36 @@ let plantilla =    {
 
 
 
-const generarCartas = () =>{
+const generarCartas = (category) =>{
 
-    productsBox.innerHTML= `
-    
-        <div class="c-card">
+    let productsFilterByCategory = productos.filter(product => product.category == category)
+
+    let cartas = ""
+
+    productsFilterByCategory.forEach(product =>{
+        cartas+=`
+        <div class="c-card" idproduct = "${product.id}">
             <div class="c-card__header">
-                <img src="./assets/img/esponja.png" alt="" class="c-card__img">
+                <img src="./assets/img/${product.id}.png" alt="" class="c-card__img">
                 <img src="#" alt="" class="c-card__like">
             </div>
             
             <div class="c-card__body">
-                <div class="c-card__name">SUPER GREEN CONCENTRATE</div>
-                <div class="c-card__description"></div>
-                <div class="c-card__price">10,49</div>
+                <div class="c-card__name">${product.name}</div>
+                <div class="c-card__description">${product.sort_description}</div>
+                <div class="c-card__price">${product.price}€</div>
                 <div class="c-card__btn"><a href="#" class="c-button c-button--add-cart">Añadir a la bolsa</a></div>
             </div>
         </div>
+        `
 
-        <div class="c-card">
-            <div class="c-card__header">
-            <img src="./assets/img/esponja.png" alt="" class="c-card__img">
-                <img src="#" alt="" class="c-card__like">
-            </div>
+    })
 
-            <div class="c-card__body">
-                <div class="c-card__name">Ecoderma</div>
-                <div class="c-card__description">Hidratante e iluminador 500ML</div>
-                <div class="c-card__price">4,49 €</div>
-                <div class="c-card__btn"><a href="#" class="c-button c-button--add-cart">Añadir a la bolsa</a></div>
-            </div>
-        </div>
+    productsBox.innerHTML = cartas
 
-        <div class="c-card">
-            <div class="c-card__header">
-            <img src="./assets/img/esponja.png" alt="" class="c-card__img">
-                <img src="#" alt="" class="c-card__like">
-            </div>
 
-            <div class="c-card__body">
-                <div class="c-card__name">Ecoderma</div>
-                <div class="c-card__description">Hidratante e iluminador 500ML</div>
-                <div class="c-card__price">4,49 €</div>
-                <div class="c-card__btn"><a href="#" class="c-button c-button--add-cart">Añadir a la bolsa</a></div>
-            </div>
-        </div>
+
     
-    
-    `
 
     productModal()
 
@@ -158,7 +170,7 @@ btnSkincare.addEventListener("click",()=>{
     btnOthers.style.borderBottom ='2px solid transparent'
     btnHaircare.style.borderBottom ='2px solid transparent'
 
-    generarCartas()
+    generarCartas("skincare")
 
 })
 
@@ -170,7 +182,7 @@ btnBodycare.addEventListener("click",()=>{
     btnOthers.style.borderBottom ='2px solid transparent'
     btnHaircare.style.borderBottom ='2px solid transparent'
 
-    generarCartas()
+    generarCartas("bodycare")
 
 })
 
@@ -183,7 +195,7 @@ btnOthers.addEventListener("click",()=>{
     btnOthers.style.borderBottom = '2px solid black'
     btnHaircare.style.borderBottom ='2px solid transparent'
 
-    generarCartas()
+    generarCartas("others")
 
 })
 
@@ -196,7 +208,8 @@ btnHaircare.addEventListener("click",()=>{
     btnOthers.style.borderBottom ='2px solid transparent'
     btnHaircare.style.borderBottom ='2px solid black' 
 
-    generarCartas()
+    generarCartas("haircare")
+
 
 })
 
