0e36492c39168eded07c2e4db5f5b64b146d8e9b
Edgar <edgarlopezcalomarde@gmail.com>
2023-01-11 15:11:22 +0100

    validaciones

--- src/web/js/validaciones.js
+++ src/web/js/validaciones.js
@@ -1,18 +1,3 @@
-
-
-/*Si coinciden las contraseñas*/
-const validatePasswords = (pass1, pass2) => pass1 === pass2
-
-
-
-
-/*
-Usuario
-Email
-Contraseña1
-Contraseña2
-*/
-
 const isEmpty = (data) =>{
     if(Object.values(data).some(v => v === "")){
         field = Object.keys(data).findIndex(it => data[it] == "")
